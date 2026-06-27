7234626a186e0ebd438ccdf90e021168c7b7acca
PolenPasteleria <cristobalaceiton4@gmail.com>
2026-06-27 06:52:45 +0200

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   const url = new URL("https://discord.com/oauth2/authorize");
   url.searchParams.set("client_id", process.env.DISCORD_CLIENT_ID);
-  url.searchParams.set("redirect_uri", process.env.DISCORD_REDIRECT_URI);
+  url.searchParams.set("redirect_uri", "https://chile-city.vercel.app/auth/callback");
   url.searchParams.set("response_type", "code");
   url.searchParams.set("scope", "identify");
   res.redirect(url.toString());
